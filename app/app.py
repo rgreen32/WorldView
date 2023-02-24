2d3e0a8e3fb2239fd7e36e507ae78110b01582fa
rgreen32 <ray.greenm@gmail.com>
2023-02-24 08:57:08 -0600

    edit to workflow

--- app/app.py
+++ app/app.py
@@ -1,4 +1,8 @@
 from app import app
+import os
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", ssl_context='adhoc')
+    cert_pem_path = os.getenv("CERT_CERT_PATH", "cert.pem")
+    key_pem_path = os.getenv("KEY_CERT_PATH", "key.pem")
+
+    app.run(host="0.0.0.0", ssl_context=(cert_pem_path, key_pem_path))
